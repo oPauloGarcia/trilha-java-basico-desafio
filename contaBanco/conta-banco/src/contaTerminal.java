fd86e79cbec6d6f4b9a462a37d2dfcd2fdef4e85
paulo garcia <paulo.garcia1992@gmail.com>
2024-08-16 22:47:54 -0300

    Projeto DIO -  Conta Banco -Finalizado

--- contaBanco/conta-banco/src/contaTerminal.java
+++ contaBanco/conta-banco/src/contaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class contaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da sua conta:");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da sua agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Por gentileza, digite seu nome:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por gentileza, digite seu sobrenome:");
+        String sobrenomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o valor que deseja depositar:");
+        Double depositar = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " " + sobrenomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + depositar + " já está disponível para saque.");
+        
+
     }
 }
